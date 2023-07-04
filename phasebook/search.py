8730a5504ea29da203e75058f88411c66e2aeab2
seylu <98249191+seyLu@users.noreply.github.com>
2023-07-04 17:39:07 +0800

    feat: implement search algo

--- phasebook/search.py
+++ phasebook/search.py
@@ -26,4 +26,41 @@ def search_users(args: dict) -> list:
 
     # Implement search here!
 
-    return USERS
+    if not args:
+        return USERS
+
+    matching_users = []
+
+    id: str = args.get("id")
+    name: str = args.get("name")
+    age: int = try_int(args.get("age"))
+    occupation: str = args.get("occupation")
+
+    for user in USERS:
+        user_id: str = user["id"]
+        user_name: str = user["name"]
+        user_age: int = user["age"]
+        user_occupation: str = user["occupation"]
+
+        print(id, user_id)
+        print(name, user_name)
+        print(age, user_age)
+        print(occupation, user_occupation)
+
+        if (id is not None and id == user_id) \
+            or (name is not None and name.casefold() in user_name.casefold()) \
+            or (age is not None and age in range(user_age - 1, user_age + 2)) \
+            or (occupation is not None and occupation.casefold() in user_occupation.casefold()) \
+        :
+            matching_users.append(user)
+
+    return matching_users
+
+
+def try_int(num: str|None):
+    if num is not None:
+        try:
+            num = int(num)
+        except ValueError:
+            num = None
+        return num
